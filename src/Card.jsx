f752b8a166f639e9fcedd48dfa8f29b72e3156ba
lasi23 <lsilvere39@gmail.com>
2026-05-04 12:14:04 +0200

    fix: changement de l'image

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,7 @@ import './Card.css'
 
 function Card() {
     return <article className='card'>
-        <img src="https://picsum.photos/200" alt="" />
+        <img className='bob' src="src\assets\Gemini_Generated_Image_qm7obtqm7obtqm7o.png" alt="" />
         <h2>BoB</h2>
         <h3>eponge@carre.net</h3>
         <p>C'est une éponge carré</p>
