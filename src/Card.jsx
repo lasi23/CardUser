68f67575f6ae4276ac6df74e7dc4152f7e35dce4
lasi23 <lsilvere39@gmail.com>
2026-05-11 20:24:13 +0200

    fix: changement du nom des class sexe dans css

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@ import './Card.css'
 function Card({user}) {
 
 
-    return <article className={'card'+user.sexe}>
+    return <article className={'card '+user.sexe}>
         <img src={user.image} alt="avatar" />
         <h2>{user.pseudo}</h2>
         <h3>{user.email}</h3>
